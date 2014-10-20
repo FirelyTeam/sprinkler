adb3a9771f0541a6f8fc810c487aa846ec8918b5
Martijn Harthoorn <m.harthoorn@furore.com>
2014-10-20 14:25:44 +0200

    Fixed copyright on some files

--- Fhir.Testing/Framework/TestHelper.cs
+++ Fhir.Testing/Framework/TestHelper.cs
@@ -1,10 +1,18 @@
-﻿using System;
+﻿/* 
+ * Copyright (c) 2014, Furore (info@furore.com) and contributors
+ * See the file CONTRIBUTORS for details.
+ * 
+ * This file is licensed under the BSD 3-Clause license
+ * available at https://raw.github.com/furore-fhir/sprinkler/master/LICENSE
+ */
+
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
-using Furore.Utilities;
+using Furore.Attributes;
 
 namespace Sprinkler.Framework
 {
